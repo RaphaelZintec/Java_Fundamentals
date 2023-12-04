e3e9e97df1a193dd658103d8428d4af8c7e50a31
Raphael <Raphael.Zolotarev@student.intecbrussel.be>
2023-12-04 09:18:41 +0100

    a

---
+++ Chapter13_Collections/src/Test/Car.java
@@ -0,0 +1,25 @@
+package Test;
+
+import java.util.Objects;
+
+public class Car {
+    private int a;
+    private String b;
+
+    public Car(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car car)) return false;
+        return a == car.a && Objects.equals(b, car.b);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+}
---
+++ Chapter13_Collections/src/Test/Main.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class Main {
+    public static void main(String[] args) {
+        Car c1 = new Car(5,"Hi");
+        Car c2 = new Car(5,"Hi");
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+    }
+}
