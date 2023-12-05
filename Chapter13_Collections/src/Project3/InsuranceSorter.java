4109e3bc258d06d62c7e47600180fda4224f6df1
raphael <raphaelzintec@gmail.com>
2023-12-05 04:19:25 +0100

    d

--- Chapter13_Collections/src/Project3/InsuranceSorter.java
+++ Chapter13_Collections/src/Project3/InsuranceSorter.java
@@ -6,5 +6,5 @@ public class InsuranceSorter implements Comparator<Patient> {
     @Override
     public int compare(Patient o1, Patient o2) {
         return Boolean.compare(o1.isEnsured(), o2.isEnsured());
-    }
+    } ///
 }
